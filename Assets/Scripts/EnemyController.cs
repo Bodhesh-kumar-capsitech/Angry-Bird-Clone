ec92b3045563cbdd39aeab8a76cf0cb61e26cdda
Bodhesh-kumar-capsitech <bodhesh.kumar@capsitech.com>
2025-11-25 18:16:04 +0530

    fixed

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,40 +1,13 @@
-using System.Linq;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    private GameObject[] enemyBirds;
-
-    void Start()
-    {
-        enemyBirds = GameObject.FindGameObjectsWithTag("Enemy");
-        print("Birds count is: " + enemyBirds.Length);
-    }
-
-    void Update()
-    {
-        
-        if(enemyBirds! == null)
-        {
-            return;
-        }
-
-        if(enemyBirds == null)
-        {
-            Time.timeScale = 0;
-            GameManager2.manager.OnLevelCompleted();
-        }
-    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Player"))
         {
-            // gameObject.SetActive(false);
             Destroy(gameObject);
         }
     }
-    
-
-
     
 }
--- Assets/Scripts/GameManager2.cs
+++ Assets/Scripts/GameManager2.cs
@@ -1,18 +1,103 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager2 : MonoBehaviour
 {
     [SerializeField] private GameObject levelCompletePannel;
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject pauseButton;
+    [SerializeField] private GameObject exitButton;
+
+
+    private GameObject[] enemies;
     
     public static GameManager2 manager;
+    private bool isLevelCompleted = false;
+    private bool isPaused = false;
+
+
 
     void Awake()
     {
         manager = this;
     }
+    void Update()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        print("Enemy length is: " + enemies.Length);
+
+        if(enemies.Length == 0)
+        {
+            OnLevelCompleted();
+        }
+        else
+        {
+            isLevelCompleted = false;
+        }
+
+        if(isLevelCompleted == true || isPaused == true) 
+        {
+           Time.timeScale = 0;   
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
     public void OnLevelCompleted()
     {
+        isLevelCompleted = true;
         levelCompletePannel.gameObject.SetActive(true);
+    }
+
+    public void OnReplay()
+    {
+        int lastLevelIndex = PlayerPrefs.GetInt("SelectedLevel");
+        LevelLoader.loader.levelIndex = lastLevelIndex;
+        SceneManager.LoadScene(1);
+        levelCompletePannel.gameObject.SetActive(false);
+    }
+
+    public void OnNext()
+    {
+       int currLevelIndex = PlayerPrefs.GetInt("SelectedLevel") + 1;
+       PlayerPrefs.SetInt("SelectedLevel",currLevelIndex);
+    //    LevelLoader.loader.levelIndex = currLevelIndex;
+       SceneManager.LoadScene(1);
+       levelCompletePannel.gameObject.SetActive(false);
+    }
+
+    public void OnHome()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnPause()
+    {
+        isPaused = true;
+        pauseButton.gameObject.SetActive(false);
+        playButton.gameObject.SetActive(true);
+        exitButton.gameObject.SetActive(true);
+    }
+
+    public void OnPlay()
+    {
+        isPaused = false;
+        pauseButton.gameObject.SetActive(true);
+        playButton.gameObject.SetActive(false);
+        exitButton.gameObject.SetActive(false);
+    }
+
+    public void OnExit()
+    {
+        #if UNITY_EDITOR
+            // Stops play mode in the Unity Editor
+            EditorApplication.isPlaying = false;
+        #else
+            // Quits the game application
+            Application.Quit();
+        #endif        
     }
 
 }
